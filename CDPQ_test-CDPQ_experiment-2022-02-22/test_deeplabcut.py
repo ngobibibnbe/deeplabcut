fa5a734c1515aa8acbf8e7573c59ecaffe463376
ngobibibnbe <anne.ngobibinbe@gmail.com>
2022-04-19 16:03:48 -0400

    ok

--- CDPQ_test-CDPQ_experiment-2022-02-22/test_deeplabcut.py
+++ CDPQ_test-CDPQ_experiment-2022-02-22/test_deeplabcut.py
@@ -2,29 +2,39 @@ import deeplabcut
 config_path= path_config_file = '/home/ulaval.ca/amngb2/projects/ul-val-prj-def-erpaq33/sophie/cdpq/deeplabcut/CDPQ_test-CDPQ_experiment-2022-02-22/config.yaml'
 
 #deeplabcut.check_labels(path_config_file)
-deeplabcut.create_multianimaltraining_dataset(
+"""deeplabcut.create_multianimaltraining_dataset(
     path_config_file,
     num_shuffles=1,
     net_type="dlcrnet_ms5",
-)
+)"""
 #deeplabcut.train_network(path_config_file, shuffle=1, max_snapshots_to_keep=5, displayiters=100, saveiters=200, maxiters=500)
-deeplabcut.train_network(
+"""deeplabcut.train_network(
     config_path,
     saveiters=300000,
     maxiters=500000,
     allow_growth=True,
 )
-
-deeplabcut.evaluate_network(
+"""
+"""deeplabcut.evaluate_network(
     config_path,
     plotting=True,
-)
+)"""
 videos=["/home/ulaval.ca/amngb2/projects/ul-val-prj-def-erpaq33/sophie/cdpq/deeplabcut/CDPQ_test-CDPQ_experiment-2022-02-22/videos/GR77_20200512_111314.mp4"]
 deeplabcut.analyze_videos(
     config_path,
     videos,
     auto_track=True
 )
+
+deeplabcut.convert_detections2tracklets(
+    config_path,
+    videos,
+    videotype='mp4',
+    shuffle=0,
+    track_method="ellipse",
+    #ignore_bodyparts=['head', 'lear', 'rear'],  # Some body parts can optionally be ignored during tracking for better assembly (but they are used later)
+)
+
 deeplabcut.stitch_tracklets(
     config_path,
     videos,
